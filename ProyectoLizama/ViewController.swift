470a1373f294c0253bb9c5b0dce1eef0da5c0cad
user178474 <user178474@74-80-239-214.ic2net.net>
2020-10-12 10:14:57 -0700

    Primer commit

---
+++ ProyectoLizama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoLizama
+//
+//  Created by user178474 on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
