dfd32353d993be1f970e2af27574044b72c19b7a
user178474 <user178474@74-80-239-214.ic2net.net>
2020-10-12 19:48:41 -0700

    Final E2

--- ProyectoLizama/ViewController.swift
+++ ProyectoLizama/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
